e9b7c95088ce0942976752c89de9316e2342d2cd
uchidaEditorial <kouei28poiou@icloud.com>
2022-09-13 10:25:00 +0900

    Cheanged 2022/09/13

--- src/test02.java
+++ src/test02.java
@@ -1,3 +1,17 @@
-public class test02 {
+import java.util.Scanner;
 
+public class test02 {
+    public static void main(String[] args) throws Exception
+
+    {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+
+        for (int i = n; i >= 0; i--)
+        {
+            System.out.printf("%d\n", i);
+        }
+    }
 }
+
